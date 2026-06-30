eec7ef4a45ae9e4ee294525331ab7b44a8aa6528
Althea Jhanne Ortiz <aeortiz@feudiliman.edu.ph>
2026-06-30 22:52:01 +0800

    Fix Context import casing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../Context/AuthContext";
 import logo from "../assets/creaid.jpg";
 import "../styles/Sidebar.css";
 
